7f461892b8ba6fb05a3b7340c68fe72705744eab
LewJun <lewjun072@sina.com>
2018-12-14 15:15:17 +0800

    EmpJpaRepo 单元测试

--- src/test/java/com/lewjun/SpringJunitTest.java
+++ src/test/java/com/lewjun/SpringJunitTest.java
@@ -4,24 +4,22 @@
  */
 package com.lewjun;
 
+import com.lewjun.bean.Emp;
 import com.lewjun.mapper.EmpJpaRepo;
+import com.lewjun.service.EmpService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.lewjun.bean.Emp;
-import com.lewjun.service.EmpService;
-
 /**
- * 
  * @author LewJun
  * @version $Id: SpringJunitTest.java, v 0.1 2017年4月13日 下午2:26:50 LewJun Exp $
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 //配置了@ContextConfiguration注解并使用该注解的locations属性指明spring和配置文件之后，
-@ContextConfiguration(locations = { "classpath:spring/spring.xml" })
+@ContextConfiguration(locations = {"classpath:spring/spring.xml"})
 public class SpringJunitTest {
     @Autowired
     private EmpService empService;
@@ -31,10 +29,14 @@ public class SpringJunitTest {
 
     @Test
     public void testEmpServiceInsert() {
-//        empService.insert(new Emp());
+        empService.insert(new Emp());
+    }
+
+    @Test
+    public void testEmpJpaRepo() {
         Emp emp = new Emp();
         empJpaRepo.save(emp);
         System.out.println(emp);
-        empJpaRepo.delete(1);
+        empJpaRepo.delete(3);
     }
 }
